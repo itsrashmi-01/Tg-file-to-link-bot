7fba33e53c7f52a56ddb784931fa2bcdcd06c120
I'TS RASHMI <itsrashmi4@gmail.com>
2026-02-16 16:24:16 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,12 +1,11 @@
-# bot/__init__.py
 from pyrogram import Client
 from config import Config
 
-# Initialize the Client
 bot_client = Client(
     "enterprise_bot",
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
     bot_token=Config.BOT_TOKEN,
-    plugins=dict(root="bot/plugins") # This loads your plugins automatically
+    plugins=dict(root="bot/plugins")
 )
+
