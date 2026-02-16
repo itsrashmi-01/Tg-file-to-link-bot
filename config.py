740620e9e2102a6aad7e52a151a803e36895659a
I'TS RASHMI <itsrashmi4@gmail.com>
2026-02-16 14:49:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
     MONGO_URI = os.environ.get("MONGO_URI", "")
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL_ID", 0))
     # Your Blogger Page URL (e.g., https://mybot.blogspot.com/p/download.html)
-    BLOG_URL = os.environ.get("BLOG_URL", "") 
+    BLOG_URL = os.environ.get("BLOG_URL", "https://tg-file-to-link.blogspot.com/2026/02/download.html") 
     # Force Sub Channel ID (Start with -100)
     FSUB_ID = int(os.environ.get("FSUB_ID", 0))
     ADMINS = [int(x) for x in os.environ.get("ADMINS", "").split()]
