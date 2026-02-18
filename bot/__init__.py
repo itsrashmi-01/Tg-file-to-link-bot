861e2e4ce6065d5b08fd413abc67290797dda45e
I'TS RASHMI <itsrashmi4@gmail.com>
2026-02-18 19:02:58 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,16 +1,1 @@
-from pyrogram import Client
-from motor.motor_asyncio import AsyncIOMotorClient
-from config import Config
 
-# Initialize MongoDB
-db_client = AsyncIOMotorClient(Config.DB_URL)
-db = db_client[Config.DB_NAME]
-
-# Initialize Main Bot
-Bot = Client(
-    "DirectLinkBot",
-    api_id=Config.API_ID,
-    api_hash=Config.API_HASH,
-    bot_token=Config.BOT_TOKEN,
-    plugins=dict(root="bot/plugins")
-)
