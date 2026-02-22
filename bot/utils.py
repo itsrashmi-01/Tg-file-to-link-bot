43cf789c7a9967ceb390b9bd172db0b9f8894990
I'TS RASHMI <itsrashmi4@gmail.com>
2026-02-22 23:19:46 +0530

    Update utils.py

--- bot/utils.py
+++ bot/utils.py
@@ -1,18 +1,15 @@
 from pyrogram import Client
-from typing import Union
-from bot.clone import db
+from config import Config
 
 class TgFileStreamer:
     def __init__(self, client: Client, file_id: str, start_offset: int = 0):
         self.client = client
         self.file_id = file_id
         self.start_offset = start_offset
-        self.bytes_yielded = 0
 
     async def __aiter__(self):
         async for chunk in self.client.stream_media(
             self.file_id,
             offset=self.start_offset
         ):
-            self.bytes_yielded += len(chunk)
             yield chunk
