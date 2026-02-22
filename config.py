01a1c1ed6b10b06a31bb3b5017d23bd4ceb54816
I'TS RASHMI <itsrashmi4@gmail.com>
2026-02-22 23:39:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    API_ID = int(os.getenv("API_ID", "0"))
+    def _get_int(key, default):
+        try:
+            return int(os.getenv(key, default))
+        except ValueError:
+            return default
+
+    API_ID = _get_int("API_ID", 0)
     API_HASH = os.getenv("API_HASH", "")
     BOT_TOKEN = os.getenv("BOT_TOKEN", "")
     
@@ -13,17 +19,19 @@ class Config:
     DB_NAME = os.getenv("DB_NAME", "Cluster0")
     
     # Channels & Admins
-    try:
-        LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID", "0"))
-    except ValueError:
-        LOG_CHANNEL_ID = 0
-        
-    FORCE_SUB_CHANNEL = int(os.getenv("FORCE_SUB_CHANNEL", "0")) 
+    LOG_CHANNEL_ID = _get_int("LOG_CHANNEL_ID", 0)
+    FORCE_SUB_CHANNEL = _get_int("FORCE_SUB_CHANNEL", 0) 
     FORCE_SUB_URL = os.getenv("FORCE_SUB_URL", "")
     
-    ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split()] if os.getenv("ADMIN_IDS") else []
+    # Admins List
+    ADMIN_IDS = []
+    if os.getenv("ADMIN_IDS"):
+        try:
+            ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS").split()]
+        except:
+            pass
 
     # Web
-    PORT = int(os.getenv("PORT", 8080))
+    PORT = _get_int("PORT", 8080)
     BASE_URL = os.getenv("BASE_URL", "http://localhost:8080").rstrip("/")
     BLOGGER_URL = os.getenv("BLOGGER_URL", "").rstrip("/")
