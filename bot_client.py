d02d6f02f27618ae803a348e3695efc48263ed35
I'TS RASHMI <itsrashmi4@gmail.com>
2026-02-22 21:28:51 +0530

    Update bot_client.py

--- bot_client.py
+++ bot_client.py
@@ -1,17 +1,8 @@
-import asyncio
-import uvloop
+from pyrogram import Client
 from config import Config
 
-# --- CRITICAL FIX START ---
-asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
-# --- CRITICAL FIX END ---
-
-from pyrogram import Client
-
 # Initialize the Main Bot
-# RENAMED TO 'tg_bot' TO AVOID CONFLICT WITH 'bot' FOLDER
+# We do NOT set the event loop policy here because Uvicorn handles it.
 tg_bot = Client(
     "FastStreamBot",
     api_id=Config.API_ID,
