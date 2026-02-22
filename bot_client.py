c081e7889a1f5e3ac34fece84e678be39625ae53
I'TS RASHMI <itsrashmi4@gmail.com>
2026-02-23 01:28:16 +0530

    Update bot_client.py

--- bot_client.py
+++ bot_client.py
@@ -1,19 +1,9 @@
-import asyncio
-import uvloop
+from pyrogram import Client
 from config import Config
 
-# --- CRITICAL FIX START ---
-asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
-# --- CRITICAL FIX END ---
-
-from pyrogram import Client
-
-# Initialize the Main Bot
-# RENAMED TO 'tg_bot' TO AVOID CONFLICT WITH 'bot' FOLDER
+# Main Bot Client (Initialized but not started)
 tg_bot = Client(
-    "FastStreamBot",
+    "main_bot",
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
     bot_token=Config.BOT_TOKEN,
