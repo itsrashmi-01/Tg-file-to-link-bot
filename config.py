8fac5b79188f1bd7d34b921005d9af8a08b5ec19
I'TS RASHMI <itsrashmi4@gmail.com>
2026-02-23 01:26:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,34 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    def _get_int(key, default):
-        try: return int(os.getenv(key, default))
-        except: return default
-
-    API_ID = _get_int("API_ID", 0)
+    API_ID = int(os.getenv("API_ID", "0"))
     API_HASH = os.getenv("API_HASH", "")
     BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-    
-    # --- Database 1: Main Bot ---
-    MONGO_URL = os.getenv("MONGO_URL") or os.getenv("DB_URL", "")
-    DB_NAME = os.getenv("DB_NAME", "Cluster0")
-    
-    # --- Database 2: Clone Bots ---
-    # If CLONE_MONGO_URL is not set, it defaults to the same cluster as Main but different DB Name
-    CLONE_MONGO_URL = os.getenv("CLONE_MONGO_URL") or MONGO_URL
-    CLONE_DB_NAME = os.getenv("CLONE_DB_NAME", "CloneCluster")
-    
-    # Channels
-    LOG_CHANNEL_ID = _get_int("LOG_CHANNEL_ID", 0)
-    
-    # Admins
-    ADMIN_IDS = []
-    raw_admins = os.getenv("ADMIN_IDS", "")
-    if raw_admins:
-        try: ADMIN_IDS = [int(x) for x in raw_admins.split()]
-        except: pass
-
-    # Web
-    PORT = _get_int("PORT", 8080)
-    BASE_URL = os.getenv("BASE_URL", "http://localhost:8080").rstrip("/")
-    BLOGGER_URL = os.getenv("BLOGGER_URL", "").rstrip("/")
+    MONGO_URL = os.getenv("MONGO_URL", "")
+    CLONE_MONGO_URL = os.getenv("CLONE_MONGO_URL", "")
+    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "0"))
+    DOMAIN = os.getenv("DOMAIN", "http://localhost:8000")
+    ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
