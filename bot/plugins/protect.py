e9766f8ae622701a4ae6ee4add8e97670e798c08
I'TS RASHMI <itsrashmi4@gmail.com>
2026-02-22 23:50:08 +0530

    Update protect.py

--- bot/plugins/protect.py
+++ bot/plugins/protect.py
@@ -3,18 +3,9 @@ from bot.clone import db
 
 @Client.on_message(filters.command("protect") & filters.private & filters.reply)
 async def protect_handler(client, message):
-    if len(message.command) < 2:
-        return await message.reply("Usage: Reply to a file with `/protect your_password`")
-    
+    if len(message.command) < 2: return await message.reply("Usage: `/protect password` (reply to file)")
     password = message.command[1]
-    target_msg = message.reply_to_message
-    
-    await message.reply(f"🔒 **Password Set:** `{password}`", quote=True)
-    
-    media = target_msg.document or target_msg.video or target_msg.audio
+    media = message.reply_to_message.document or message.reply_to_message.video or message.reply_to_message.audio
     if media:
-        await db.files.update_one(
-            {"file_unique_id": media.file_unique_id},
-            {"$set": {"password": password}},
-            upsert=True
-        )
+        await db.files.update_one({"file_unique_id": media.file_unique_id}, {"$set": {"password": password}})
+        await message.reply("🔒 Password set!")
