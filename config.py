98d6271880985658b6334ed0d89f20d4cc6a1476
I'TS RASHMI <itsrashmi4@gmail.com>
2026-02-22 23:46:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
+    # Helper to safely get integers
     def _get_int(key, default):
-        try:
-            return int(os.getenv(key, default))
-        except ValueError:
-            return default
+        try: return int(os.getenv(key, default))
+        except: return default
 
     API_ID = _get_int("API_ID", 0)
     API_HASH = os.getenv("API_HASH", "")
@@ -18,18 +17,15 @@ class Config:
     MONGO_URL = os.getenv("MONGO_URL") or os.getenv("DB_URL", "")
     DB_NAME = os.getenv("DB_NAME", "Cluster0")
     
-    # Channels & Admins
+    # Channels
     LOG_CHANNEL_ID = _get_int("LOG_CHANNEL_ID", 0)
-    FORCE_SUB_CHANNEL = _get_int("FORCE_SUB_CHANNEL", 0) 
-    FORCE_SUB_URL = os.getenv("FORCE_SUB_URL", "")
     
-    # Admins List
+    # Admins (Space separated)
     ADMIN_IDS = []
-    if os.getenv("ADMIN_IDS"):
-        try:
-            ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS").split()]
-        except:
-            pass
+    raw_admins = os.getenv("ADMIN_IDS", "")
+    if raw_admins:
+        try: ADMIN_IDS = [int(x) for x in raw_admins.split()]
+        except: pass
 
     # Web
     PORT = _get_int("PORT", 8080)
