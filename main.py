d22e07251ed876356a4101d96a0ff3608dfc779d
I'TS RASHMI <itsrashmi4@gmail.com>
2026-02-23 10:35:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import asyncio
 import uvicorn
-from fastapi import FastAPI
 from bot_client import tg_bot
 from bot.clone import load_all_clones
 from bot.server.stream_routes import router as stream
 from bot.server.auth_routes import router as auth
+from fastapi import FastAPI
 
 app = FastAPI()
 app.include_router(stream)
 app.include_router(auth)
 
-async def start():
+async def start_services():
+    # Start the bot
     await tg_bot.start()
+    # Load clones
     await load_all_clones()
-    config = uvicorn.Config(app, host="0.0.0.0", port=8080)
+    
+    # Setup and run the server
+    config = uvicorn.Config(app, host="0.0.0.0", port=8080, loop="asyncio")
     server = uvicorn.Server(config)
     await server.serve()
 
 if __name__ == "__main__":
-    asyncio.run(start())
+    asyncio.run(start_services())
